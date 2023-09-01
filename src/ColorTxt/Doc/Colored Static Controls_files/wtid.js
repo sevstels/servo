28d0ae2bb7e9c1ae6cf3f1b98eab4935b61f2198
sevstels <decoder@rambler.ru>
2023-09-01 23:46:04 +0800

    -

--- src/ColorTxt/Doc/Colored Static Controls_files/wtid.js
+++
@@ -1,3 +0,0 @@
-if (typeof(Webtrends.dcss.dcsobj_0.dcsGetIdCallback)=="function"){
-Webtrends.dcss.dcsobj_0.dcsGetIdCallback({"gWtId":"137.132.3.10-1191054672.30620893","gTempWtId":"","gWtAccountRollup":"1"});
-}
