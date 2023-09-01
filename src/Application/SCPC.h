41300f4a48d595e516564f6205fd2db9f737e228
sevstels <decoder@rambler.ru>
2023-09-01 23:28:55 +0800

    --

--- src/Application/SCPC.h
+++ src/Application/SCPC.h
@@ -2,12 +2,12 @@
 //File name:   "SCPC.h"
 //Purpose:      Header File	for the PROJECT_NAME application
 //Version:      1.00
-//Copyright:    (c) 2017, Akimov Vladimir  E-mail: decoder@rambler.ru		
+//Copyright:    (c) 2023, Akimov Vladimir  E-mail: decoder@rambler.ru		
 //==============================================================================
 #pragma once
 
 #ifndef __AFXWIN_H__
-	#error "include 'stdafx.h' before including this file for PCH"
+#error "include 'stdafx.h' before including this file for PCH"
 #endif
 
 // main symbols
@@ -17,12 +17,12 @@
 // See SCPC.cpp for the implementation of this class
 class CSCPCApp : public CWinApp
 {
-public:
-	CSCPCApp();
+  public:
+  CSCPCApp();
 
-// Overrides
-public:
-	virtual BOOL InitInstance();
+  // Overrides
+  public:
+  virtual BOOL InitInstance();
   // Implementation
   DECLARE_MESSAGE_MAP()
 };
